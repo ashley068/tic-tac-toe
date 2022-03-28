bb21b1ce8a821065d98ca9dfa429b2b40e6ab8b6
ashley68 <ashley0608ly@gmail.com>
2022-03-28 13:22:57 -0400

    tictactoe

---
+++ script.js
@@ -0,0 +1,45 @@
+let count = 0,
+  rows = [0, 0, 0],
+  cols = [0, 0, 0],
+  diag = 0,
+  anti_diagonal = 0,
+  player;
+const message = document.getElementById("winner");
+
+function playerClicked(i, j, element) {
+  if (
+    element.style.backgroundColor != "yellow" &&
+    element.style.backgroundColor != "red"
+  ) {
+    count++;
+    player = count % 2 ? 1 : -1;
+    if (player === 1) {
+      element.style.backgroundColor = "yellow";
+    } else if (player === -1) {
+      element.style.backgroundColor = "red";
+    }
+    rows[i] += player;
+    cols[j] += player;
+    if (i === j) diag += player;
+    if (i + j === 2) anti_diagonal += player;
+
+    if ([rows[i], cols[j], diag, anti_diagonal].includes(3)) {
+      message.innerHTML = "Player1 wins!!";
+      TimeoutFunction();
+    } else if ([rows[i], cols[j], diag, anti_diagonal].includes(-3)) {
+      message.innerHTML = "Player2 wins!!";
+      TimeoutFunction();
+    }
+    if (count === 9) {
+      message.innerHTML = "draw";
+      TimeoutFunction();
+    }
+  } else return;
+}
+let gameTimeout;
+function TimeoutFunction() {
+  clearTimeout(gameTimeout);
+  gameTimeout = setTimeout(() => {
+    location.reload();
+  }, 2000);
+}
